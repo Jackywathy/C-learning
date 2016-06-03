596b27aa1b24694be48051705fbbb31386755574
Jack Jiang <jackywathy24@gmail.com>
2016-06-03 17:24:03 +1000

    worked on ghellowkr

--- helloworld.c
+++ helloworld.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
 void ex_3_3(void){
     int a, b, c, d;
     double root;
@@ -104,11 +105,88 @@ void ex_3_6(void){
 }
 }
 
+void ex_3_7(void){
+    char sign;
+    int degree;
+    double out = 0;
+    printf("Enter a temperature(F or C): ");
+    scanf("%d%c", &degree, &sign);
+    if (sign == 'F'){
+        out = degree - 32;
+        out = (out * 5)/ 9;
+        printf("The temperature %dF converts to %.2f%c",degree, out, 'C');
+    }
+    else if (sign == 'C'){
+        out = degree;
+        out = out/9;
+        out = out * 5;
+        out += 32;
+        printf("The temperature %dC converts to %.2f%c",degree, out, 'F');
+    }
+
+}
+
+/* converter */
+void ex_3_8(void){
+    char unit;
+    float item;
+    printf("Enter a quantity: ");
+    scanf("%f%c", &item,&unit);
+    if (unit=='M'){
+        printf("The distance %.1f miles converts to %.1f kilometers", item, item*1.609);
+    }
+    else if (unit=='K'){
+        printf("The distance %.1f kilomenters converts to %.1f miles", item, item/1.609);
+    }
+    else if (unit=='P'){
+        printf("The weight %.1f pounds converts to %.1f kilograms", item, item*0.454);
+    }
+    else if (unit=='G'){
+        printf("The weight %.1f kilograms converts to %.1f pounds", item, item/0.454);
+    }
+
+}
+/* number adder */
+void number_reader(void){
+    int input;
+    int counter = 0;
+    printf("Enter a series of numbers seperated by spaces, follwed by control+d: ");
+    while (scanf("%d", &input) == 1){
+        counter += input;
+    }
+    printf("The numbers add up to: %d", counter);
+}
+
+/* fibanacii formula */
+int fib(int days){
+    int iteration,first,second,temp;
+    for (iteration = 1;iteration <= days; iteration++){
+        if (iteration == 1||iteration == 2){
+            first = 1;
+            second = 1;
+        }
+        else{
+            temp = second+first;
+            first = second;
+            second = temp;
+        }
+    }
+    return second;
+
+}
+
+/* fib-driver */
+void ex_4_3(void){
+    int input;
+    printf("Calculates the nth fibanaci number: ");
+    scanf("%d", &input);
+    printf("%d", fib(input));
+}
 
 int main(void){
     int rand;
-    ex_3_6();
-    /* scanf("%d", &rand); */
+    ex_4_3();
+    scanf("%s", &rand);
     return 0;
 
 }
